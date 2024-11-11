a48ae2e8a7449f756d7dec9d6cbe3aa8049de1fe
sinking_sea_wheat <52875009+SinkingSeaWheat@users.noreply.github.com>
2024-11-11 18:02:04 +0900

    なぜかContext側でpageを作成してから渡すとエラーになるので、Scenarioでpageを作成

--- src/component/headlessBrowser/Context.ts
+++ src/component/headlessBrowser/Context.ts
@@ -72,11 +72,10 @@ class Context {
       this.browser = await chromium.launch({headless:true});
     }
     const context = await this.browser.newContext(contextOption);
-    const page = await context.newPage();
     const {urlsToOpen, ...otherOption} = this.soption;
     urlsToOpen.forEach((url)=>{
       if(context !== null){
-        this.scenarios.push(new Scenario(this.note.createPageResult(url), page, otherOption));
+        this.scenarios.push(new Scenario(this.note.createPageResult(url), context, otherOption));
       }
     });
     this.scenarioQueue.on('idle', async ()=>{
