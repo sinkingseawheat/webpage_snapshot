af6b4fe26dc76438f8b2b7759d601f56d8395b23
sinking_sea_wheat <52875009+SinkingSeaWheat@users.noreply.github.com>
2024-11-22 17:22:26 +0900

    テキストファイル一覧の表の縦と横を逆にした

--- src/component/snapshot/Renderer/DescriptionOfTextFile.tsx
+++ src/component/snapshot/Renderer/DescriptionOfTextFile.tsx
@@ -7,49 +7,47 @@ const DescriptionOfTextFile:React.FC<{
   results:MergedResultItem[],
   getPath:ReturnType<typeof setGetPathToSendFile>
 }> = ({results, getPath})=>{
-  return (<>{
-    results.map(({
-      requestURL, type, tagName, relURL, href, responseURL, status, contentType, contentLength, shaHash, source, linkSourceIndex, archiveIndex, errorMessage
-    })=>{
-      if(contentType === undefined || contentType === null){return null;}
-      const query = (()=>{
-        if(archiveIndex !== null || archiveIndex !== undefined){
-          // return `contentType=${encodeURIComponent(contentType)}`;
-          return `contentType=${encodeURIComponent('text/plain; charset=UTF-8')}`;
-        }
-        else{
-          // アーカイブされていないとき
-          return '';
-        }
-      })();
-      return (<div key={requestURL}>
-        <table className={style.imageItem__table}>
-            <tbody>
-              <tr><th>URL（リクエスト）</th><td>{archiveIndex !== null ? <Link target="_blank" className={style.textLink} href={getPath(`archive/${archiveIndex}?${query}`)}>{requestURL}</Link> : requestURL}</td></tr>
-              <tr><th>URL（サーバーリダイレクト後）</th><td>{requestURL === responseURL ? 'サーバーリダイレクト無' : responseURL}</td></tr>
-              <tr><th>レスポンスステータス</th><td>{status}</td></tr>
-              <tr><th>ハッシュ（sha-256）</th><td>{shaHash ?? `取得できません`}</td></tr>
-              <tr><th>Content-Type</th><td>{contentType ?? `取得できません`}</td></tr>
-              <tr><th>Content-Length</th><td>{
-              contentLength === -1 || contentLength === undefined || contentLength === null ?
-                '取得できません'
-                : contentLength.toString().replace(/(\d+?)(?=(\d{3})+(?!\d))/g,'$1,')+'byte'
-              }</td></tr>
-              <tr><th>データの取得方法</th><td>{
-                source === 'extracted' ? 'ファイルのURLへの直接アクセス'
-                : source === 'requestedFromPage' ? 'ページからのリクエスト'
-                : '判別できませんでした'
-              }</td></tr>
-              {
-                errorMessage !== '' && errorMessage !== undefined ?
-                <tr><th>エラーメッセージ</th><td>{errorMessage}</td></tr>
-                : ''
-              }
-            </tbody>
-          </table>
-      </div>);
-    }).filter((jsxElm)=>jsxElm!==null)
-  }</>);
+  return (<div className={style.table}>
+    <table>
+      <thead>
+        <tr>
+          <th>URL（リクエスト）</th>
+          <th>URL（サーバーリダイレクト後）</th>
+          <th>レスポンスステータス</th>
+          <th>ハッシュ（sha-256）</th>
+          <th>Content-Type</th>
+          <th>Content-Length</th>
+          <th>データの取得方法</th>
+          <th>エラーメッセージ</th>
+        </tr>
+      </thead>
+      <tbody>
+        {results.map(({
+    requestURL, type, tagName, relURL, href, responseURL, status, contentType, contentLength, shaHash, source, linkSourceIndex, archiveIndex, errorMessage
+  })=>{
+        return (<tr key={requestURL}>
+          <td className={style["table__data--url"]}>{archiveIndex !== null && archiveIndex !== undefined ? <Link target="_blank" className={style.textLink} href={getPath(`archive/${archiveIndex}?contentType=${encodeURIComponent('text/plain; charset=UTF-8')}`)}>{requestURL}</Link> : requestURL}</td>
+          <td className={style["table__data--url"]}>{requestURL === responseURL ? 'サーバーリダイレクト無' : responseURL}</td>
+          <td>{status ?? '取得できません'}</td>
+          <td className={style["table__data--url"]}>{shaHash ?? `取得できません`}</td>
+          <td>{contentType ?? `取得できません`}</td>
+          <td>{
+          contentLength === -1 || contentLength === undefined || contentLength === null ?
+            '取得できません'
+            : contentLength.toString().replace(/(\d+?)(?=(\d{3})+(?!\d))/g,'$1,')+'byte'
+          }</td>
+          <td>{
+            source === 'extracted' ? 'ファイルのURLへの直接アクセス'
+            : source === 'requestedFromPage' ? 'ページからのリクエスト'
+            : '判別できませんでした'
+          }</td>
+          <td>{errorMessage !== '' && errorMessage !== undefined ? errorMessage : '無し'}</td>
+        </tr>);
+        })}
+        <tr></tr>
+      </tbody>
+    </table>
+  </div>);
 }
 
 export {DescriptionOfTextFile}
